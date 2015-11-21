f02139c8bc84869f47e1430dce8f54ffd39dcdf6
liranpi2@gmail.com <liranpi2@gmail.com>
2015-11-22 00:26:32 +0200

    creating dynamic ui elements based on json file

--- app/src/main/java/com/main/trainer/dogtrainer/CustomAdapter.java
+++ app/src/main/java/com/main/trainer/dogtrainer/CustomAdapter.java
@@ -5,7 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -47,16 +53,41 @@ public class CustomAdapter extends BaseAdapter {
             // get view
             currentView = inflater.inflate(R.layout.each_list_item, parent, false);
         }
+
         Response.QuestionsEntity item = (Response.QuestionsEntity) getItem(position);
 
-        // get view elements
-        TextView question = (TextView) currentView.findViewById(R.id.questionText);
-        TextView answer = (TextView) currentView.findViewById(R.id.questionType);
+        LinearLayout ll = (LinearLayout)currentView.findViewById(R.id.linearLayout2);
 
-        // set view elements
+        // set question
+        TextView question = new TextView(mContext);
         question.setText(item.getQuestion());
-        answer.setText(item.getAnswer());
+        ll.addView(question);
 
+        // set answer by type
+        switch (item.getAnswer())
+        {
+            case "text":
+                // create textbox
+                EditText et = new EditText(mContext);
+                et.setText(item.getAnswer());
+                ll.addView(et);
+                break;
+
+            case "radio":
+                // create radio button
+                final RadioButton[] rb = new RadioButton[5];
+                RadioGroup rg = new RadioGroup(mContext); //create the RadioGroup
+                rg.setOrientation(RadioGroup.HORIZONTAL);//or RadioGroup.VERTICAL
+                for(int i=0; i<2; i++){
+                    rb[i]  = new RadioButton(mContext);
+                    rb[i].setText(item.getAnswer() + i);
+                    rb[i].setId(i);
+                    rg.addView(rb[i]); //the RadioButtons are added to the radioGroup instead of the layout
+
+                }
+                ll.addView(rg);//you add the whole RadioGroup to the layout
+                break;
+        }
         return currentView;
 
     }
--- app/src/main/java/com/main/trainer/dogtrainer/MainActivity.java
+++ app/src/main/java/com/main/trainer/dogtrainer/MainActivity.java
@@ -22,6 +22,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
